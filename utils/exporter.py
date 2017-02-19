61b2266cbd70eacf1382f3a6c46dd16485e4f7e7
Feilan Jiang <fjiang120@gmail.com>
2017-02-19 01:51:35 -0500

    Fix naming of output dir and file names

--- utils/exporter.py
+++ utils/exporter.py
@@ -3,10 +3,10 @@ from os import makedirs
 
 from utils.names import output_file_name
 
-_out_dir = 'graphs/'
+_out_dir = 'graphs'
 
 def export(fig, module, dates):
-    graph_dir = '{}{}/'.format(_out_dir, str(module))[:-3]  # remove .py extension from dir names
+    graph_dir = '{}/{}'.format(_out_dir, module[:-3]) # remove .py extension from dir names
     makedirs(graph_dir, exist_ok=True)
-    py.offline.plot(fig, filename=graph_dir + output_file_name(module, dates))
+    py.offline.plot(fig, filename='{}/{}'.format(graph_dir, output_file_name(module, dates)))
 
